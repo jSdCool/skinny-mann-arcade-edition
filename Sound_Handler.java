fcdbb7d3491fea4b2fdf7e52d72cd52e3b5ec0ea
jSdCool <37940266+jSdCool@users.noreply.github.com>
2023-11-14 23:45:47 -0500

    added narration sound type to sound handler

--- Sound_Handler.java
+++ Sound_Handler.java
@@ -1,20 +1,20 @@
-//version 2.0.0
+//version 2.1.0
 import processing.core.*;
 import processing.sound.*;
 import java.util.ArrayList;
 class SoundHandler extends Thread {
-  SoundFile[] music[], queue, sounds;
-  ArrayList<SoundFile> levelSounds = new ArrayList<>();
+  SoundFile[] music[], queue, sounds,narrations;
+  ArrayList<SoundFile> levelSounds = new ArrayList<>(),levelNarrations = new ArrayList<>();
   private SoundFile []cSound=new SoundFile[3];//sound queue
   PApplet ggn;
   private int musNum=0, currentMusicTrack=0, trackToSwitchTo=0;
-  private float masterVolume=1, musicVolume=1, sfxVolume=1, prevVol=1;
+  private float masterVolume=1, musicVolume=1, sfxVolume=1, prevVol=1,narrationVolume=1;
   private boolean switchMusicTrack=false, keepAlive=true, enableSounds=false, startMusic=false;
 
 
-  private SoundHandler(String[][] musicFiles, String[] soundsFiles, PApplet X) {
+  private SoundHandler(String[][] musicFiles, String[] soundsFiles, String[] narrationFiles,PApplet X) {
     music=new SoundFile[musicFiles.length][];
-    for (int i=0; i<musicFiles.length; i++) {//set the size of the nusic tracks
+    for (int i=0; i<musicFiles.length; i++) {//set the size of the music tracks
       music[i]=new SoundFile[musicFiles[i].length];
     }
     queue = new SoundFile[8];//create the sound queue
@@ -28,6 +28,10 @@ class SoundHandler extends Thread {
       for (int j=0; j<musicFiles[i].length; j++) {
         music[i][j]=new SoundFile(X, musicFiles[i][j]);//load the music files
       }
+    }
+    narrations = new SoundFile[narrationFiles.length];
+    for(int i=0;i<narrations.length;i++){
+      narrations[i] = new SoundFile(X,narrationFiles[i]);//load the narrations
     }
     start();//start the independednt sound handler thread
   }
@@ -158,6 +162,10 @@ class SoundHandler extends Thread {
   public void setSoundsVolume(float volume) {
     sfxVolume=volume;
   }
+  
+  public void setNarrationVolume(float volume){
+    narrationVolume = volume;
+  }
 
   public static Builder builder(PApplet a) {
     return new Builder(a);
@@ -182,6 +190,13 @@ class SoundHandler extends Thread {
     SoundFile sound = new SoundFile(ggn, path);
     int id =sounds.length+levelSounds.size();
     levelSounds.add(sound);
+    return id;
+  }
+  
+  public int registerLevelNarration(String path){
+    SoundFile sound = new SoundFile(ggn, path);
+    int id = narrations.length+levelNarrations.size();
+    levelNarrations.add(sound);
     return id;
   }
 
@@ -227,12 +242,48 @@ class SoundHandler extends Thread {
       }
     }
   }
+  
+  public void playNarration(int n){
+    SoundFile sound;
+    if (n<narrations.length) {
+      sound=narrations[n];
+    } else {
+      sound=levelNarrations.get(n-narrations.length);
+    }
+    sound.play(1, masterVolume*narrationVolume);
+  }
+  
+  public boolean isNarrationPlaying(int n){
+    if (n<narrations.length) {
+      return narrations[n].isPlaying();
+    } else {
+      return levelNarrations.get(n-narrations.length).isPlaying();
+    }
+  }
+  
+  public void stopNarration(int n){
+    SoundFile s;
+    if (n<narrations.length) {
+      s= narrations[n];
+    } else {
+      s= levelNarrations.get(n-narrations.length);
+    }
+
+    if (s.isPlaying()) {
+      s.stop();
+      return;
+    }
+  }
 
   public void dumpLS() {//dump level sounds and allow them to be garbage collected
     for (int i=0; i<levelSounds.size(); i++) {
       levelSounds.get(i).removeFromCache();
     }
+    for(int i=0;i<levelNarrations.size();i++){
+      levelNarrations.get(i).removeFromCache();
+    }
     levelSounds = new ArrayList<>();
+    levelNarrations = new ArrayList<>();
     System.gc();//run garbage collection to remove old uloaded sound files from memory
   }
 
@@ -246,6 +297,7 @@ class SoundHandler extends Thread {
     PApplet window;
     private ArrayList<ArrayList<String>> musicPaths=new ArrayList<>();
     private ArrayList<String> soundPaths=new ArrayList<>();
+    private ArrayList<String> narrationPaths = new ArrayList<>();
     private int numMusicTracks=1;
 
     public Builder addMusic(String path, int track) {
@@ -259,6 +311,18 @@ class SoundHandler extends Thread {
       soundPaths.add(path);
       return this;
     }
+    
+    public Builder addNarration(String path,int[] narrationIdCallBack){
+      if(narrationIdCallBack != null && narrationIdCallBack.length >0){
+        narrationIdCallBack[0] = narrationPaths.size();
+      }
+      narrationPaths.add(path);
+      return this;
+    }
+    
+     public Builder addNarration(String path){
+       return addNarration(path,null);
+     }
 
     public Builder addMusicTrack() {
       numMusicTracks++;
@@ -273,11 +337,12 @@ class SoundHandler extends Thread {
     public SoundHandler build() {
       String[] sounds=soundPaths.toArray(new String[]{});
       String[][] music=new String[numMusicTracks][];
+      String[] narrations = new String[]{};
       for (int i=0; i<numMusicTracks; i++) {
         music[i]=musicPaths.get(i).toArray(new String[]{});
       }
 
-      return new SoundHandler(music, sounds, window);
+      return new SoundHandler(music, sounds, narrations, window);
     }
   }
 }
